1a40b20592ffd9902ef60e4ea76b9084fe57d412
Aryan Salemabadi <aryanavatar1380@gmail.com>
2025-04-09 04:56:03 +0330

    final about commit

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -5,7 +5,7 @@ import AboutUs from '@/components/template/AboutUs/AboutUs'
 
 const AboutUsPage = () => {
   return (
-    <main className="min-h-screen bg-gray-50 py-36 lg:py-40 md:py-28 sm:py-36">
+    <main className="min-h-screen py-28 lg:py-24 md:py-28 sm:py-24">
       <AboutUs />
     </main>
   )
