c1ccf345f37310d9cb595a401a0732eb1f45f181
Aryan Salemabadi <aryanavatar1380@gmail.com>
2025-04-15 23:44:13 +0330

    add another sections

--- src/services/getAllImages/index.ts
+++ src/services/getAllImages/index.ts
@@ -9,7 +9,7 @@ export const fetchAllImages = async () => {
                 "Content-Type": "application/json",
             },
             next: {
-                revalidate: 60,
+                revalidate: 20,
             }
         }
         );
